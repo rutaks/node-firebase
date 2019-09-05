e870d2e9a18444767b9bcbaef44c1df68119bae7
rutaks <rootsum.dev@gmail.com>
2019-09-05 19:54:24 +0100

    Added Error Message On Login

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const firebase = require('firebase');
+const firebase = require("firebase");
 
-const config = require('../keys');
+const config = require("../keys");
 
 firebase.initializeApp(config);
 
-router.get('/login', (req, res) => {
-	res.render('users/login');
+router.get("/login", (req, res) => {
+  res.render("users/login");
 });
 
-router.post('/login', (req, res) => {
-	firebase
-		.auth()
-		.signInWithEmailAndPassword(req.body.email, req.body.password)
-		.then((user) => {
-			res.redirect('/');
-		})
-		.catch((error) => {
-			let errCode = error.code;
-			let errMsg = error.message;
-			console.log('>' + errCode + ', ' + errMsg);
-			res.redirect('/users/login');
-		});
+router.post("/login", (req, res) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then(user => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      let errCode = error.code;
+      let errMsg = error.message;
+      console.log(">" + errCode + ", " + errMsg);
+      res.render("users/login", { msg: errMsg });
+    });
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.redirect('/users/login');
+router.get("/", function(req, res, next) {
+  res.redirect("/users/login");
 });
 
 module.exports = router;
